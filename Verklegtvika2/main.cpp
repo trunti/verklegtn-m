cd9d074055400a68edaee0aa5e80d54b2a4ca638
Svanurjoh <svanurjoh15@ru.is>
2015-12-08 13:43:04 +0000

    Virkar sko. Bara ekki alveg

--- Verklegtvika2/main.cpp
+++ Verklegtvika2/main.cpp
@@ -1,10 +1,13 @@
 #include "ui/ui.h"
 #include "data/data.h"
+#include "data/scientists.h"
 
 int main()
 {
     Data data;
     UI ui;
+    Scientists sci;
+    sci.addScientist();
     ui.mainMenu();
 
     return 0;

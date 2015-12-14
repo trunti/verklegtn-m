c2950b4503da874683edb17435f17eb5b45f61e5
Svanurjoh <svanurjoh15@ru.is>
2015-12-14 22:49:17 +0000

    Listar

--- EndurvinnslustöðATeam/Skil3/Ui/linktogether.cpp
+++ EndurvinnslustöðATeam/Skil3/Ui/linktogether.cpp
@@ -6,9 +6,44 @@ LinkTogether::LinkTogether(QWidget *parent) :
     ui(new Ui::LinkTogether)
 {
     ui->setupUi(this);
+    displayAllScientists();
+    displayAllComputers();
 }
 
 LinkTogether::~LinkTogether()
 {
     delete ui;
+}
+void LinkTogether::displayAllScientists()
+{
+    scientName = scientistService.getAllScientists("pName", true);
+    DisplayScientName(scientName);
+}
+void LinkTogether::DisplayScientName(vector<Scientist> scientName)
+{
+    ui->list_scient->clear();
+
+    for(unsigned int i = 0;i < scientName.size();i++)
+    {
+        Scientist currentScient = scientName[i];
+
+        ui->list_scient->addItem(QString::fromStdString(currentScient.getName()));
+
+    }
+}
+void LinkTogether::displayAllComputers()
+{
+    compName = computerService.getAllComputers("Name", true);
+    DisplayCompName(compName);
+}
+void LinkTogether::DisplayCompName(vector<Computer> compName)
+{
+    ui->list_comp->clear();
+
+    for(unsigned int i = 0;i < compName.size();i++)
+    {
+        Computer currentComp = compName[i];
+
+        ui->list_comp->addItem(QString::fromStdString(currentComp.getName()));
+    }
 }
--- EndurvinnslustöðATeam/Skil3/Ui/linktogether.h
+++ EndurvinnslustöðATeam/Skil3/Ui/linktogether.h
@@ -2,6 +2,10 @@
 #define LINKTOGETHER_H
 
 #include <QDialog>
+#include "Services/computerservice.h"
+#include "Services/scientistservice.h"
+#include <vector>
+#include <string>
 
 namespace Ui {
 class LinkTogether;
@@ -16,7 +20,16 @@ public:
     ~LinkTogether();
 
 private:
+    void displayAllScientists();
+    void displayAllComputers();
+    void DisplayScientName(vector<Scientist> scientName);
+    void DisplayCompName(vector<Computer> compName);
+    vector<Scientist> scientName;
+    vector<Computer> compName;
     Ui::LinkTogether *ui;
+
+    Scientistservice scientistService;
+    Computerservice computerService;
 };
 
 #endif // LINKTOGETHER_H
